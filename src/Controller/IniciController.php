d8230175441f21c4db60b854951dc5ab84fcf71a
edcarbonellsimarro <edcarbonellsimarro@gmail.com>
2023-11-14 13:30:04 +0100

    Commit despres de merge

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -4,11 +4,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class IniciController
 {
-    #[Route('/' ,name:'inici')]
+    //#[Route('/' ,name:'inici')]
 public function inici()
 {
 // mostra un missatge de benvinguda amb un objecte Response
 return new Response("Benvinguda al web de contactes");
-}
+}  
 }
 ?>
